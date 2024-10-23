02044a2cf60e528a57a9823d1a34c1120770277a
Sharad Mathukiya <saradmathukiya9@gmail.com>
2024-10-23 14:49:29 +0530

    backend origin

--- backend/app.js
+++ backend/app.js
@@ -20,8 +20,7 @@ config({
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
-    // origin: "http://localhost:5173",
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true,
   })
